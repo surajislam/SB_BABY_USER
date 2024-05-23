a857b94a8c554c66c88ec82881777fd6db430959
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-23 20:06:05 +0530

    Update alive.py

--- X/modules/user/alive.py
+++ X/modules/user/alive.py
@@ -91,7 +91,7 @@ async def alip(client: Client, message: Message):
         f"{emoji} <b>𝐏ʏᴛʜᴏ𝐍 𝐕ᴇʀꜱɪᴏ𝐍:</b> <code>{python_version()}</code> \n\n"
         f"{emoji} <b>𝐆ʀᴏᴜ𝐏 :</b> [𝐒ᴜᴘᴘᴏʀ𝐓](https://t.me/Mrpasserby_1227)** \n\n"
         f"{emoji} <b>𝐂ʜᴀɴɴᴇʟ:<b> [𝐔ᴘᴅᴀᴛᴇ𝐒](https://t.me/Seethistoknow)** \n\n"
-        f"{emoji} <b>[𝐃ᴇᴘʟᴏʏ](http://dashboard.heroku.com/new?template=https://github.com/Shubhxspam/Japanese-X-Userbot) 𝐘ᴏᴜʀ 𝐎ᴡɴ [𝐉𝐀𝐏𝐀𝐍𝐄𝐒𝐄-𝐗-𝐔𝐒𝐄𝐑𝐁𝐎𝐓](http://github.com/Team-Japanese/Japanese-X-Userbot) ✧\n\n"
+        f"{emoji} <b>[𝐃ᴇᴘʟᴏʏ](http://dashboard.heroku.com/new?template=https://github.com/Shubhxspam/Japanese-X-Userbot) 𝐘ᴏᴜʀ 𝐎ᴡɴ [𝐒𝐇𝐔𝐁𝐇-𝐗-𝐔𝐒𝐄𝐑𝐁𝐎𝐓](http://github.com/Shubhxspam/Japanese-X-Userbot) ✧\n\n"
         
     )
     try:
